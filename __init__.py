6955ab42131916200353dc0a17f50f11888e0b76
SeanLewisIRE <slewis100@gmail.com>
2020-07-14 12:23:17 +0100

    added support to allow scraping using chrome

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
-print('Hello from Python')
+from bs4 import BeautifulSoup
+import urllib.request
+from selenium import webdriver
+from chromedriver_py import binary_path
+from webdriver_manager.chrome import ChromeDriverManager
+
+print('Running Script...')
+
+
+
+url = "https://www.tesco.ie/groceries/product/browse/default.aspx?N=4294954026&Ne=4294954028"
+
+# chrome session
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url)
+
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, "html")
+
+
+
+print(soup)
+print('End Script...')
