723fce067485c138f9ea6f6c500017d185b11784
SeanLewisIRE <slewis100@gmail.com>
2020-07-15 16:10:00 +0100

    Updated Makefile. Added code for Chrome to run headlessly

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import chromedriver_binary
@@ -15,7 +16,26 @@ start_url = "https://www.tesco.ie/groceries/product/browse/default.aspx?N=429495
 tidy_prices = []
 
 # chrome session
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--window-size=1280x1696')
+chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+chrome_options.add_argument('--hide-scrollbars')
+chrome_options.add_argument('--enable-logging')
+chrome_options.add_argument('--log-level=0')
+chrome_options.add_argument('--v=99')
+chrome_options.add_argument('--single-process')
+chrome_options.add_argument('--data-path=/tmp/data-path')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--homedir=/tmp')
+chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+#chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+""" ^ line tells Lambda where to find the headless Chrome binary file"""
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.get(start_url)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
