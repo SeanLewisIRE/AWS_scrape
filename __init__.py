15f44d2bd9753ea228a507deefb706bd811a0aa6
SeanLewisIRE <slewis100@gmail.com>
2020-07-15 10:28:32 +0100

    Added comments

--- __init__.py
+++ __init__.py
@@ -6,26 +6,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 print('Running Script...')
 
-#urls
+#Starting URL
 start_url = "https://www.tesco.ie/groceries/product/browse/default.aspx?N=4294954026&Ne=4294954028"
 
-
+#list in global scope
+tidy_prices = []
 
 # chrome session
-
-
-tidy_prices = []
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(start_url)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 def scrape_page():
+    """Scrapes each page individually, cleans and adds data to list"""
     product_prices = soup.find_all("span", class_="linePrice")
-
     for price in product_prices:
         tidy_prices.append(price.get_text())
 
 def scraper():
+    """Initiates scraper, iterates over 9 pages"""
     scrape_page()
     for pages in range(9):
         scrape_page()
@@ -33,6 +32,7 @@ def scraper():
         next_button.click()
     driver.quit()
 
+#Call scraper
 scraper()
 
 print(tidy_prices)
